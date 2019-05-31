60fe3320aa7deb4bb301c743cd9d34444bbb6ac6
Dmytro Moseichenko <dmym@sitecore.net>
2019-05-31 19:09:54 +0300

    submit actions

---
+++ SpiritSite/SubmitActions/SubmitQuotation.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using SpiritSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpiritSite.SubmitActions
+{
+    public class SubmitQuotation : SubmitActionBase<Quotation>
+    {
+        public SubmitQuotation(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        protected override bool Execute(Quotation data, FormSubmitContext formSubmitContext)
+        {
+            
+            return true;
+        }
+
+        protected override bool TryParse(string value, out Quotation target)
+        {
+            target = new Quotation();
+            return true;
+        }
+    }
+}
